23c8e31f8e32b8991a1ddd2b2af4ade72dd613ec
ASUS <k.zezgn@gmail.com>
2022-10-12 19:07:12 +0300

    x

---
+++ src/test/java/pages/KenanPageDelete.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class KenanPageDelete {
+    public static void main(String[] args) {
+        System.out.println("silmelik class gonderdim");
+
+
+        System.out.println("branch kenan a gectim 19.00");
+
+    }
+}
