2d092cc56723c04fb19ce89e0f6eb53036b2bf56
Ramazan Kuyucu <kuyucur@gmail.com>
2022-10-13 10:33:31 +0300

    13.10

--- src/test/java/pojos/LoginPagePojo.java
+++ src/test/java/pojos/LoginPagePojo.java
@@ -1,5 +1,6 @@
 package pojos;
 
+import io.cucumber.core.internal.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
