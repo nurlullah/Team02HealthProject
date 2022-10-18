41fe74190138b594285cb4270de7fdf535c40f59
Nurullah Demirkoparan <nurlullahhh@gmail.com>
2022-10-18 08:09:16 +0300

    clear

--- src/test/java/pojos/LoginPagePojo.java
+++
@@ -1,9 +0,0 @@
-package pojos;
-
-import io.cucumber.core.internal.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-
-public class LoginPagePojo {
-
-}
